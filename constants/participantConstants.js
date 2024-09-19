3d25391e7cfbbcba66bd876d16be97c07a38ca72
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-19 11:48:37 +0300

    feat: added constants for participants

---
+++ constants/participantConstants.js
@@ -0,0 +1,4 @@
+export const regexFullName = /^[A-Z][a-zA-Z'-]+(?: [A-Z][a-zA-Z'-]+)+$/;
+export const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+export const sourceList = ["Social media", "Friends", "Found myself"];
